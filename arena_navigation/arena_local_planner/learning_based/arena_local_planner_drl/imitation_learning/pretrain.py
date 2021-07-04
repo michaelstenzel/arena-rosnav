938288934a30c441dfe4169b3da85af237d18665
Michael Stenzel <michael.e.stenzel@gmail.com>
2021-07-04 22:43:13 +0200

    fixed train/test split, changed logging to batch and epoch logging

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrain.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrain.py
@@ -15,12 +15,12 @@ from torch.utils.data.dataset import random_split
 from stable_baselines3 import PPO
 
 from rl_agent.envs.flatland_gym_env import FlatlandEnv
-from dataset import EpisodeDataset, MapDataset
+from dataset import MapDataset
 
 from tensorboardX import SummaryWriter
 
 
-def pretrain(agent, map_dataset, num_epochs=1, batch_size=32, gamma=0.7, learning_rate=0.05, oversample=False):
+def pretrain(agent, map_dataset, num_epochs=1, batch_size=32, gamma=0.7, learning_rate=1.0):
     # the map data_set is a list of EpisodeDatasets
     # each EpisodeDatasets is a list of tuples: (observations, actions)
 
@@ -39,88 +39,79 @@ def pretrain(agent, map_dataset, num_epochs=1, batch_size=32, gamma=0.7, learnin
 
     loss_fn = nn.MSELoss() # use mean-squared error loss for regression
     # initialize optimizer
-    optimizer = optim.Adadelta(network.parameters(), lr=learning_rate)  #TODO experiment with SGD, RMSprop and Adagrad
+    optimizer = optim.Adadelta(network.parameters(), lr=learning_rate)  #TODO experiment with Adadelta and Adam
     # initialize learning rate scheduler
-    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)  #TODO understand hyperparameters step_size, gamma
+    #scheduler = StepLR(optimizer, step_size=1, gamma=gamma)  #TODO experiment with optim.lr_scheduler.ReduceLROnPlateau
 
-    running_training_loss = 0
-    running_test_loss = 0
-    training_step_counter = 0
-    test_step_counter = 0
+    # do 70%/30% train/test split with a random seed for reproducibility
+    training_set_size = int(0.7*len(map_dataset))
+    test_set_size = len(map_dataset) - training_set_size
+    training_set, test_set = random_split(map_dataset, [training_set_size, test_set_size], generator=th.Generator().manual_seed(0))
+    
+    # batch counters: count+1 for each batch loaded from the training or test set
+    # used only to log the batch loss
+    training_batch_counter = 0
+    test_batch_counter = 0
     for epoch in range(num_epochs):
-        # train and test sets should come from the same episode/path!
-        # iterate over the episodes in the map dataset
-        episode_counter = 1
-        for episode in map_dataset:
-            # naive oversampling (will result in overlap of test and train set)
-            if oversample:
-                start = int(0.3*len(episode))
-                episode.actions = np.append(episode.actions, episode.actions[0:start], axis=0)
-                episode.observations = np.append(episode.observations, episode.observations[0:start], axis=0)
-                #TODO alternative: could create a dataset out of the start arrays, concatenated with test set...
-            #print(f"training on episode {episode_counter}/{len(map_dataset)}")
-            # do 70% train, 30% test split for the episode  #TODO is there a scikitlearn function for this?
-            training_set_size = int(0.7*len(episode))
-            test_set_size = len(episode) - training_set_size
-
-            training_set, test_set = random_split(episode, [training_set_size, test_set_size])
-
-            train_loader = th.utils.data.DataLoader(dataset=training_set, batch_size=batch_size, shuffle=True)  #TODO how do these work under the hood?
-            test_loader = th.utils.data.DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
-
-            # iterate over the timesteps of the training set:
-            network.train()
-            for batch, (observation, action) in enumerate(train_loader):
+        # TRAINING LOOP
+        # iterate over the episodes of the training set:
+        epoch_training_loss = 0  # keep running total of loss over the current epoch
+        network.train()
+        for episode in training_set:
+            # instantiate dataloader for this episode
+            episode_loader = th.utils.data.DataLoader(dataset=episode, batch_size=batch_size, shuffle=True)
+            # iterate over timesteps in this episode
+            for batch, (observation, action) in enumerate(episode_loader):
                 optimizer.zero_grad()
-                action_network, _, _ = network(observation)  # run forward pass  #TODO check number of outputs
+                action_network, _, _ = network(observation)  # run forward pass to get network's prediction  #TODO check number of outputs
                 #print(f"action_network: {action_network}")
                 action_network = action_network.double()  # MSELoss() expects inputs to be doubles
                 #print(f"action_network.double(): {action_network}")
                 loss = loss_fn(action_network, action)  # compute loss
+
+                epoch_training_loss += loss.item()
+                # log average batch loss: total loss in this batch normalized by the number of timesteps in the batch
+                writer.add_scalar('training batch loss', loss.item()/len(observation), training_batch_counter)
+                training_batch_counter += 1
+
                 loss.backward()  # backpropagate loss
                 optimizer.step()  # adjust network parameters using the losses  #TODO check this
+                
+        # average training loss for the epoch: running total of loss over the epoch divided by number of samples in training_set
+        writer.add_scalar('training epoch loss', epoch_training_loss/len(training_set), epoch)
+        print(f"epoch {epoch}/{num_epochs} | training epoch loss: {epoch_training_loss/len(training_set)}")
 
-                running_training_loss += loss.item()  # keep running tally of training loss over the last 1000 iterations
-                if training_step_counter % 100 == 99:  # every 1000 iterations, log training loss
-                    writer.add_scalar('running training loss', running_training_loss/100, training_step_counter)
-                    print(f"epoch {epoch}/{num_epochs} | running training loss: {running_training_loss/100}")
-                    running_training_loss = 0
-                training_step_counter += 1
-            #average_train_loss = train_loss / len(training_set)
-            #print(f"epoch: {epoch} | episode {episode_counter}/{len(map_dataset)} | average training set loss: {average_train_loss}")
-            
-            # iterate over the timesteps of the test set to get the average test set loss
-            network.eval()  #TODO don't update gradients? Need both eval and no_grad?
-            with th.no_grad():  # don't update gradients!
-                for batch, (observation, action) in enumerate(test_loader):
+        # TEST LOOP
+        # iterate over the episodes of the test set to get the average test set loss
+        epoch_test_loss = 0
+        network.eval()  #TODO don't update gradients? Need both eval and no_grad?
+        with th.no_grad():  # don't update gradients!
+            for episode in test_set:
+                # instantiate dataloader for this episode
+                episode_loader = th.utils.data.DataLoader(dataset=episode, batch_size=batch_size, shuffle=True)
+                # iterate over timesteps in this episode
+                for batch, (observation, action) in enumerate(episode_loader):
                     action_network, _, _ = network(observation)
                     action_network = action_network.double()
-                    running_test_loss += loss_fn(action_network, action).item()  #TODO check me
+                    loss = loss_fn(action_network, action)  #TODO check me
                     
-                    if test_step_counter % 100 == 99:
-                        writer.add_scalar('running test loss', running_test_loss/100, test_step_counter)
-                        print(f"epoch {epoch}/{num_epochs} | running test loss: {running_test_loss/100}")
-                        running_test_loss = 0
-                    test_step_counter += 1
-                #average_loss = loss / len(test_set)
-                #print(f"epoch: {epoch} | episode {episode_counter}/{len(map_dataset)} | average test set loss: {average_loss}")
-
-            episode_counter += 1
-        scheduler.step()
+                    epoch_test_loss += loss.item()
+                    # log average batch loss: total loss in this batch normalized by the number of timesteps in the batch
+                    writer.add_scalar('test batch loss', loss.item()/len(observation), test_batch_counter)
+                    test_batch_counter += 1
+                    
+            # average test loss for the epoch: running total of loss over the epoch divided by number of samples in test_set
+            writer.add_scalar('test epoch loss', epoch_test_loss/len(test_set), epoch)
+            print(f"epoch {epoch}/{num_epochs} | test epoch loss: {epoch_test_loss/len(test_set)}")
+        #scheduler.step()  # reduce the learning rate at the end of the episode
     
-    agent.policy = network  # overwrite agent's network with the new pretrained one
-    
-    #writer.add_graph(network)  #TODO pass an observation (mini-batch) to add_graph() 
-    #File "/home/michael/python_env/rosnav/lib/python3.6/site-packages/torch/utils/tensorboard/__init__.py", line 4, in <module>
-    #raise ImportError('TensorBoard logging requires TensorBoard version 1.15 or above')
-    
+    agent.policy = network  # overwrite agent's network with the new pretrained one    
     writer.close()
     return agent
 
 
 if __name__ == '__main__':  
-    # create map dataset(s)
-    #map_dataset = MapDataset('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/output/map_empty_small/')
+    # need dummy scenario to instantiate FlatlandEnv. Need a gym env to instantiate a PPO agent.
     scenario = '/home/michael/catkin_ws/src/arena-rosnav/gui/eval_scenarios/0_done_json_files/eval2_with_map_empty_small/07.0__2_static_2_dynamic_obs_robot_pos1.json'
     models_folder_path = rospkg.RosPack().get_path('simulator_setup')
     arena_local_planner_drl_folder_path = rospkg.RosPack().get_path('arena_local_planner_drl')
@@ -136,18 +127,19 @@ if __name__ == '__main__':
                                move_base_simple=True
                   )
 
-    # create map datasets
+    # create map dataset
     #map_dataset = MapDataset('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/output/map_empty_small')
     map_dataset = MapDataset('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/output/')
 
-    #TODO create PPO agent
+    # create PPO agent
     ppo_agent = PPO('MlpPolicy', env, verbose=1)  #TODO verbose=1?
     date_str = datetime.now().strftime('%Y%m%d_%H-%M')
     ppo_agent.save(f'baseline_ppo_agent_{date_str}')  # save untrained agent to use as a baseline
 
-    #call pretrain() function
-    trained_agent = pretrain(ppo_agent, map_dataset, num_epochs=8, oversample=False)
+    # pretrain the PPO agent
+    trained_agent = pretrain(ppo_agent, map_dataset, num_epochs=40, batch_size=32, learning_rate=1.0)
     print("trained agent!")
-    #TODO return value: model. save as pth file for evaluation, running OR save the ppo agent as done above
+
+    # save the pretrained PPO agent
     date_str = datetime.now().strftime('%Y%m%d_%H-%M')
     trained_agent.save(f'pretrained_ppo_agent_{date_str}')
