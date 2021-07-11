4aa78dcf2d3ea7d52881e96917d55970fb2d3d2f
Michael Stenzel <michael.e.stenzel@gmail.com>
2021-07-11 16:32:46 +0200

    cleaned up env initialization and imports

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/run_agent.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/run_agent.py
@@ -1,25 +1,14 @@
 import os
-import rospy
 import rospkg
-from datetime import datetime
 import time
-
-import torch as th
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
-from torch.utils.data.dataset import random_split
 
 from stable_baselines3 import PPO
 
 from rl_agent.envs.flatland_gym_env import FlatlandEnv
-from dataset import EpisodeDataset, MapDataset
-
-from tensorboardX import SummaryWriter
+from dataset import MapDataset
 
 # create map dataset(s)
 map_dataset = MapDataset('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/output/map_empty_small/')
-scenario = '/home/michael/catkin_ws/src/arena-rosnav/gui/eval_scenarios/0_done_json_files/eval2_with_map_empty_small/07.0__2_static_2_dynamic_obs_robot_pos1.json'
 models_folder_path = rospkg.RosPack().get_path('simulator_setup')
 arena_local_planner_drl_folder_path = rospkg.RosPack().get_path('arena_local_planner_drl')
 
@@ -27,7 +16,6 @@ ns = ''
 
 env = FlatlandEnv(ns=ns, PATHS={'robot_setting': os.path.join(models_folder_path, 'robot', 'myrobot.model.yaml'), 'robot_as': os.path.join(arena_local_planner_drl_folder_path,
                             'configs', 'default_settings.yaml'), "model": "/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/agents/rule_00",
-                            "scenerios_json_path": scenario,
                             "curriculum": "/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/configs/training_curriculum.yaml"},
                             reward_fnc="rule_00", is_action_space_discrete=False, debug=False, train_mode=True, max_steps_per_episode=650,
                             safe_dist=None, curr_stage=1,
@@ -61,11 +49,19 @@ map_dataset = MapDataset('/home/michael/catkin_ws/src/arena-rosnav/arena_navigat
 # trained with ~40k steps + ~40k (only recording 90 steps per episode)
 #ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210616_16-05.zip', env)
 
-# trained with ~102k steps
-ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210616_19-03.zip', env)
+# trained with ~102k steps - THIS IS THE LAST ONE I INVESTIGATED!!!
+#ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210616_19-03.zip', env)
 # trained with ~111k steps
 #ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210616_21-59.zip', env)
 
+# July 4th - fixed train/test split. 8 epochs@batch_size=32
+ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210704_20-27.zip', env)
+# corresponding baseline:
+#ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/baseline_ppo_agent_20210704_20-22.zip', env)
+# July 4th - fixed train/test split. 40 epochs@batch_size=32
+#ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210704_21-01.zip', env)
+# July 4th - fixed train/test split. 40 epochs@batch_size=8
+#ppo_agent = PPO.load('/home/michael/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/pretrained_ppo_agent_20210704_22-20.zip', env)
 
 steps = 50000
 obs = env.reset()
