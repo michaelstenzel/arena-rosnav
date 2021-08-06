f029a5c9cd101531afd13d29dac48297677f5da5
Michael Stenzel <michael.e.stenzel@gmail.com>
2021-08-06 20:00:14 +0200

    add docstring to EpisodeDataset

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/dataset.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/imitation_learning/dataset.py
@@ -1,7 +1,5 @@
 import numpy as np
 import os
-
-import torch as th
 from torch.utils.data.dataset import Dataset
 
 #TODO docstring
@@ -12,8 +10,14 @@ from torch.utils.data.dataset import Dataset
 # the index of each array is the timestep in that episode
 
 class EpisodeDataset(Dataset):
-    #TODO docstring
     """ EpisodeDataset
+    contains all the (observation, action) pairs in one episode/rollout/trajectory from starting position to goal.
+    Args:
+        path_to_file (str): path (including .npz file extension) of an npz file storing two numpy arrays:
+            one called "observations" and one called "actions". The index of each array indexes the timestep in that episode.
+            It is assumed that the data is synchronized, that is that at each index
+            the elements of both arrays form one (observation, action) pair.
+            See save_episode() in human_expert.py or record_rollouts.py to see how the files are saved.
     """
     def __init__(self, path_to_file):
         data = np.load(path_to_file)
@@ -40,9 +44,7 @@ class MapDataset(Dataset):
                 self.map_dataset.append(EpisodeDataset(os.path.join(root, file)))
 
     def __getitem__(self, index):
-        # self.episodes[index]
         return self.map_dataset[index]
 
     def __len__(self):
-        # return len(self.episodes)
         return len(self.map_dataset)
